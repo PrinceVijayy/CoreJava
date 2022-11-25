e965822f5bcac2bd675407cd4f089440a3c9b4f2
PrinceVijayy <kondavijayy123@gmail.com>
2022-11-25 12:21:59 +0530

    day of week added

---
+++ CoreJavaSenarioBasedPrograms/55.COJ_DAY_OF_WEEK/src/main/java/com/ojas/model/DayOfWeek.java
@@ -0,0 +1,14 @@
+package com.ojas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DayOfWeek {
+	
+	String dayName;
+
+}
